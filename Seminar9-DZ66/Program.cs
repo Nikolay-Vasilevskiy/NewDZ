5f5579f47f21d1cbf14b9595735412b4f32c5903
Oksana <caps14@gmail.com>
2022-05-27 19:14:09 +0800

    Seminar9-DZ66

---
+++ Seminar9-DZ66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+int n, m, s=0;
+Console.Write("Введи значение M: ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение N: ");
+m = Convert.ToInt16(Console.ReadLine());
+void PrintNumbers(int n, int m, int s)
+{
+for (int i = n; i <= m; i++)
+    s += i;
+Console.WriteLine(s);
+}
+PrintNumbers(n,m,s);
